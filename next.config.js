d36b1e11451f9d40df9f01fafc0b47d449acfa19
Kevin Lam <kevinlam.sf@gmail.com>
2024-04-30 20:25:28 -0700

    add next config for styled components and images

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true
+    },
+    images: {
+        domains: ['s3-us-west-2.amazonaws.com']
+    }
+}
